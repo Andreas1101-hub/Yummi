ab928d11b5e981645dd561358b37637c6c41e545
Andreas Kwong <andreaskwong@gmail.com>
2024-01-31 17:13:33 +0000

    Finishing off adding an ingredient

--- Yummi/ContentView.swift
+++ Yummi/ContentView.swift
@@ -21,19 +21,56 @@ struct ContentView: View {
         }
     }
     @State private var newIngredientName = ""
+    @State private var newQuantity = ""
+    enum Unit: String, CaseIterable, Identifiable {
+        case kg, teaspoons, ml
+        var id: Self { self }
+    }
+    @State private var selectedUnit: Unit = .kg
+    @State private var selectedCategory: Category = .Carbs
+    @State private var newExpiryDate = Date()
     var body: some View {
         Form {
             Section {
                 VStack(alignment: .leading, spacing: 20) {
                     Text("\(ingredientsList.allIngredients[selectedIngredient].displayStats())")
-                    Button("Next Ingredient", action: {
+                    Button(action: {
                         selectedIngredient += 1
-                    })
+                    }) {
+                        Label("Next Ingredient",systemImage: "capsule.inset.filled")
+                    }
                 }
             }
             Section {
                 VStack(alignment: .leading, spacing: 20) {
                     TextField("Ingredient Name", text: $newIngredientName)
+                    HStack {
+                        TextField("Quantity", text: $newQuantity)
+                        List {
+                            Picker("", selection: $selectedUnit) {
+                                Text("Kilogram").tag(Unit.kg)
+                                Text("Tea Spoons").tag(Unit.teaspoons)
+                                Text("Milliliter").tag(Unit.ml)
+                            }
+                        }
+                    }
+                    List {
+                        Picker("Category", selection: $selectedCategory) {
+                            Text("Fruits").tag(Category.Fruits)
+                            Text("Meats").tag(Category.Meats)
+                            Text("Carbs").tag(Category.Carbs)
+                            Text("Dairy").tag(Category.Dairy)
+                            Text("Vegetables").tag(Category.Vegetables)
+                        }
+                    }
+                    DatePicker("Expiry Date", selection: $newExpiryDate)
+                    Button(action: {
+                        let tempIngredient = Ingredient(name: newIngredientName, quantity: newQuantity, unit: selectedUnit.rawValue, category: selectedCategory, expiryDate: getDate(year: 2025, month: 12, day: 5))
+                        ingredientsList.allIngredients.append(tempIngredient)
+                        print(ingredientsList.allIngredients)
+                    }) {
+                        Label("Add New Ingredient", systemImage: "checkmark.seal")
+                    }
                 }
             }
         }
--- Yummi/Model.swift
+++ Yummi/Model.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Ingredient {
     let name: String
-    var quantity: Int
+    var quantity: String
     var unit: String
     var category: Category
     var expiryDate: Date
@@ -27,16 +27,17 @@ Expiry Date: \(expiryDate.formatted(date: .abbreviated, time: .omitted))
     
 }
 
-enum Category: String {
+enum Category: String, CaseIterable, Identifiable {
     case Fruits
     case Meats
     case Dairy
     case Vegetables
     case Carbs
+    var id: Self { self }
 }
 
 struct allIngredient {
-    var allIngredients = [Ingredient(name: "Apple", quantity: 5, unit: "kg", category: .Fruits, expiryDate: getDate(year: 2025, month: 12, day: 5)),
-                          Ingredient(name: "Orange", quantity: 4, unit: "kg", category: .Fruits, expiryDate: getDate(year: 2025, month: 12, day: 6)),
-                          Ingredient(name: "Bread", quantity: 25, unit: "loaves", category: .Carbs, expiryDate: getDate(year: 2024, month: 02, day: 19))]
+    var allIngredients = [Ingredient(name: "Apple", quantity: "5", unit: "kg", category: .Fruits, expiryDate: getDate(year: 2025, month: 12, day: 5)),
+                          Ingredient(name: "Orange", quantity: "4", unit: "kg", category: .Fruits, expiryDate: getDate(year: 2025, month: 12, day: 6)),
+                          Ingredient(name: "Bread", quantity: "25", unit: "kg", category: .Carbs, expiryDate: getDate(year: 2024, month: 02, day: 19))]
 }
